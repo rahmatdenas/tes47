65687977b5179adf1ff72801ca580a29908fcb5c
rahmatdenas <120230537+rahmatdenas@users.noreply.github.com>
2026-07-01 09:27:42 +0700

    Update eph-heritage-sites.js

--- js/eph-heritage-sites.js
+++ js/eph-heritage-sites.js
@@ -1117,7 +1117,7 @@ if (record.lat !== undefined && record.lon !== undefined) {
   // Sisa perakitan HTML ke panel...
   let eventsHtmlPlaceholder = `
     <div id="events-container-${qid}" class="loading" style="margin-top: 8px; min-height: 24px;">
-      <div class="loader" style="width: 20px; height: 20px; border-width: 2px; margin: 0;"></div>
+      <div class="loader" style="width: 20px; height: 20px; border-width: 2px; margin-bottom: 10px;"></div>
     </div>`;
 
   designationsHtml +=
